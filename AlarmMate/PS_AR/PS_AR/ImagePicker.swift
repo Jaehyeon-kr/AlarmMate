467e57bf6f6995a206f8cf83cb51fe18cd64b4fa
qmdlghfl3@naver.com <qmdlghfl3@naver.com>
2025-11-23 06:59:33 +0900

    Restore ImagePicker.swift (standard SwiftUI UIImagePicker wrapper)

---
+++ AlarmMate/PS_AR/PS_AR/ImagePicker.swift
@@ -0,0 +1,42 @@
+//
+//  ImagePicker.swift
+//  PS_AR
+//
+//  Created by 심재현 on 11/22/25.
+//
+
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Environment(\.dismiss) var dismiss
+    @Binding var selectedImage: UIImage?
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePicker
+
+        init(_ parent: ImagePicker) { self.parent = parent }
+
+        func imagePickerController(
+            _ picker: UIImagePickerController,
+            didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]
+        ) {
+            if let img = info[.originalImage] as? UIImage {
+                parent.selectedImage = img
+            }
+            parent.dismiss()
+        }
+    }
+}
